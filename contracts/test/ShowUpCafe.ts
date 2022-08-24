e035120fa0e7b5caaaaf18bf00c9365738aee7ad
MRingive <mathiasringive@hotmail.com>
2022-08-24 07:19:07 +0200

    Unit tests

--- contracts/test/ShowUpCafe.ts
+++ contracts/test/ShowUpCafe.ts
@@ -34,6 +34,15 @@ describe("ShowUpCafe", function () {
       expect(await contract.balanceOf(otherAccount.address)).to.equal(1);
     });
 
+    it("should mint token from non-owner", async () => {
+      const { contract, otherAccount } = await loadFixture(contractDeployedFixture);
+
+      await contract.connect(otherAccount).safeMint(otherAccount.address, { value: ethers.utils.parseUnits("10")})
+
+      expect(await contract.ownerOf(0)).to.equal(otherAccount.address);
+      expect(await contract.balanceOf(otherAccount.address)).to.equal(1);
+    });
+
     it("should revert for missing payment", async () => {
       const { contract, owner } = await loadFixture(contractDeployedFixture);
 
@@ -47,6 +56,46 @@ describe("ShowUpCafe", function () {
       .to.be.revertedWith("Missing payment");
     });
 
+    it("should mint two tokens", async () => {
+      const { contract, owner } = await loadFixture(contractDeployedFixture);
+
+      await contract.safeMint(owner.address, { value: ethers.utils.parseUnits("10")})
+      await contract.safeMint(owner.address, { value: ethers.utils.parseUnits("10")})
+
+      expect(await contract.ownerOf(0)).to.equal(owner.address);
+      expect(await contract.ownerOf(1)).to.equal(owner.address);
+      expect(await contract.balanceOf(owner.address)).to.equal(2);
+    });
+
+    it("should mint two tokens to different accounts", async () => {
+      const { contract, owner, otherAccount } = await loadFixture(contractDeployedFixture);
+
+      await contract.safeMint(owner.address, { value: ethers.utils.parseUnits("10")})
+      await contract.safeMint(otherAccount.address, { value: ethers.utils.parseUnits("10")})
+
+      expect(await contract.ownerOf(0)).to.equal(owner.address);
+      expect(await contract.ownerOf(1)).to.equal(otherAccount.address);
+      expect(await contract.balanceOf(owner.address)).to.equal(1);
+      expect(await contract.balanceOf(otherAccount.address)).to.equal(1);
+    });
+
+    it("should pay to owner", async () => {
+      const { contract, owner, otherAccount } = await loadFixture(contractDeployedFixture);
+
+      await contract.connect(otherAccount).safeMint(otherAccount.address, { value: ethers.utils.parseUnits("10")})
+
+      expect(await contract.payments(owner.address)).to.equal(ethers.utils.parseUnits("10"));
+    });
+
+    it("should pay to owner for two mints", async () => {
+      const { contract, owner, otherAccount } = await loadFixture(contractDeployedFixture);
+
+      await contract.connect(otherAccount).safeMint(otherAccount.address, { value: ethers.utils.parseUnits("10")})
+      await contract.connect(otherAccount).safeMint(otherAccount.address, { value: ethers.utils.parseUnits("11")})
+
+      expect(await contract.payments(owner.address)).to.equal(ethers.utils.parseUnits("21"));
+    });
+    
   });
 
 });
